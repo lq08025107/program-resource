ed72af587b80b578eecc7c40b3c69ca2dca5a2b4
lq08025107 <534525962@qq.com>
2017-11-16 17:31:55 +0800

    answer

---
+++ code/src/BFind.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author liuqiang
+ *
+ */
+public class BFind {
+	public static boolean find(int[] array, int target){
+		int start = 0;
+		int end = array.length-1;
+		while(start <= end){
+			int middle = (start + end)/2;
+			if(target == array[middle]){
+				return true;
+			}
+			else if(target < array[middle]){
+				end = middle - 1;
+			}
+			else {
+				start = middle + 1;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,5,6};
+		int target = 3;
+		boolean flag = find(array, target);
+		System.out.println(flag);
+
+	}
+
+}
